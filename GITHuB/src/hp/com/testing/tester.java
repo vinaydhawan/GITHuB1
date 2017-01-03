cb4694f31490094177ed5fe948e9be7ecc7892a9
vinaydhawan <vinay.dhawan@tcs.com>
2017-01-03 18:14:27 +0530

    First Commit

---
+++ GITHuB/src/hp/com/testing/tester.java
@@ -0,0 +1,15 @@
+package hp.com.testing;
+
+import org.testng.annotations.Test;
+
+public class tester{
+	@Test
+	public void hello(){
+		System.out.println("My First Test case");
+		System.out.println("My Second Test case");
+		System.out.println("My first change");
+		//test 1234
+		//test
+	}
+	
+}
